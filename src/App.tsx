8ee14a275343869fbf006f0e43a70b965e5f11cf
Erfan Safaei <erfan.safaei.78@gmail.com>
2024-10-09 18:09:00 +0330

    feat: make routes and theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Markets } from "./pages/markets";
+import { MarketDetails } from "./pages/marketDetails";
+import { Header } from "./components/Header";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Markets />,
+  },
+  {
+    path: "market/:marketId",
+    element: <MarketDetails />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Header />
+      <div className="w-full">
+        <RouterProvider router={routes} />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,48 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
+
+interface ThemeContextType {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const recentTheme = localStorage.getItem("theme");
+    if (recentTheme === "light" || recentTheme === "dark") {
+      setTheme(recentTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
