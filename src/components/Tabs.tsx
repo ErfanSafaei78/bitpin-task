112f5ad458a565741e60dc6b9a86a59bece720d3
Erfan Safaei <erfan.safaei.78@gmail.com>
2024-10-09 18:29:54 +0330

    feat(global components): add Tabs component

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,22 @@
+type TabsProps<T extends string> = {
+  tabs: readonly T[];
+  activeTab: T;
+  setActiveTab: (activeTab: T) => void;
+};
+
+export function Tabs<T extends string>({ activeTab, setActiveTab, tabs }: TabsProps<T>) {
+  return (
+    <ul className="nav nav-tabs mb-3">
+      {tabs.map((tab) => (
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === tab ? "active" : "text-muted"}`}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
